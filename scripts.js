12087717a24f3a26083f9aee49bb9228966223ab
yasminbraga <yasminbragateixeira@gmail.com>
2019-12-28 13:10:41 -0300

    adiciona modal js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        const imgId = card.getAttribute('id')
+        const cardTitle = card.querySelector('.content-title').innerHTML
+        const cardAuthor = card.querySelector('.content-author').innerHTML
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('img').src = `/assets/${imgId}`
+        modalOverlay.querySelector('.modal-title').innerHTML = cardTitle
+        modalOverlay.querySelector('.modal-author').innerHTML = cardAuthor
+    })
+}
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove('active')
+})
